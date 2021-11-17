b451ab4f31933c942e0cd7659a7ffce4b3a44b73
Rafael Cordeiro <rafaelcordeiro70@gmail.com>
2021-11-16 22:22:54 -0300

    fix: Alteração de relacionamento many to many entre FORNECEDORES e FORNECIVEIS no banco de dados

--- database-scripts/database-ddl.sql
+++ database-scripts/database-ddl.sql
@@ -3,10 +3,10 @@
 
 -- 1
 CREATE TABLE PAISES (
-	pai_id            SERIAL      NOT NULL,
-	pai_codigo        CHAR(3)     NOT NULL,
+	pai_id            SERIAL       NOT NULL,
+	pai_codigo        CHAR(3)      NOT NULL,
 	pai_descricao     VARCHAR(255) NOT NULL,
-	pai_tipo_cadastro CHAR(1)     NOT NULL,
+	pai_tipo_cadastro CHAR(1)      NOT NULL,
 	CONSTRAINT PK_PAI PRIMARY KEY (pai_id),
 	CONSTRAINT UK_PAI_01 UNIQUE (pai_codigo),
 	CONSTRAINT CK_PAI_01 CHECK (pai_tipo_cadastro IN ('R','A','I'))
@@ -15,11 +15,11 @@ CREATE TABLE PAISES (
 
 -- 2
 CREATE TABLE ESTADOS (
-	est_id            SERIAL      NOT NULL,
-	est_codigo        CHAR(2)     NOT NULL,
+	est_id            SERIAL       NOT NULL,
+	est_codigo        CHAR(2)      NOT NULL,
 	est_descricao     VARCHAR(255) NOT NULL,
-	est_pai_id        INTEGER     NOT NULL,
-	est_tipo_cadastro CHAR(1)     NOT NULL,
+	est_pai_id        INTEGER      NOT NULL,
+	est_tipo_cadastro CHAR(1)      NOT NULL,
 	CONSTRAINT PK_EST PRIMARY KEY (est_id),
 	CONSTRAINT FK_EST_PAI FOREIGN KEY (est_pai_id)
 		REFERENCES PAISES (pai_id),
@@ -30,11 +30,11 @@ CREATE TABLE ESTADOS (
 
 -- 3
 CREATE TABLE CIDADES (
-	cid_id            SERIAL      NOT NULL,
-	cid_codigo        CHAR(8)     NOT NULL,
+	cid_id            SERIAL       NOT NULL,
+	cid_codigo        CHAR(8)      NOT NULL,
 	cid_descricao     VARCHAR(255) NOT NULL,
-	cid_est_id        INTEGER     NOT NULL,
-	cid_tipo_cadastro CHAR(1)     NOT NULL,
+	cid_est_id        INTEGER      NOT NULL,
+	cid_tipo_cadastro CHAR(1)      NOT NULL,
 	CONSTRAINT PK_CID PRIMARY KEY (cid_id),
 	CONSTRAINT FK_CID_EST FOREIGN KEY (cid_est_id)
 		REFERENCES ESTADOS (est_id),
@@ -53,7 +53,7 @@ CREATE TABLE FORNECEDORES (
 	for_tipo_logradouro VARCHAR(20)  NOT NULL,
 	for_numero          VARCHAR(6)   NOT NULL,
 	for_bairro          VARCHAR(20)  NOT NULL,
-	for_cep             VARCHAR(10)   NOT NULL,
+	for_cep             VARCHAR(10)  NOT NULL,
 	for_complemento     VARCHAR(20)  NOT NULL,
 	for_cid_id          INTEGER      NOT NULL,
 	for_tipo_cadastro   CHAR(1)      NOT NULL,
@@ -71,6 +71,7 @@ CREATE TABLE CONTATOS (
 	con_ddi           VARCHAR(4)   NOT NULL,
 	con_ddd           VARCHAR(3)   NOT NULL,
 	con_telefone      VARCHAR(12)  NOT NULL,
+	con_ramal         VARCHAR(12)  NOT NULL,
 	con_nome          VARCHAR(255) NOT NULL,
 	con_email         VARCHAR(255) NOT NULL,
 	con_departamento  VARCHAR(255) NOT NULL,
@@ -97,19 +98,32 @@ CREATE TABLE DOCUMENTOS (
 	CONSTRAINT CK_DOC_01 CHECK (doc_tipo_cadastro IN ('R','A','I'))
 );
 
+-- 6
+CREATE TABLE FORNECEDORES_FORNECIVEIS (
+	for_fnv_id      SERIAL  NOT NULL,
+	for_fnv_for_id  INTEGER NOT NULL,
+	for_fnv_fnv_id  INTEGER NOT NULL,
+	for_fnv_data_inicio DATE,
+	CONSTRAINT PK_FOR_FNV PRIMARY KEY (for_fnv_id),
+	CONSTRAINT FK_FOR_FNV_FOR FOREIGN KEY (for_fnv_for_id)
+		REFERENCES FORNECEDORES (for_id),
+	CONSTRAINT FK_FOR_FNV_FNV FOREIGN KEY (for_fnv_fnv_id)
+		REFERENCES FORNECIVEIS (fnv_id)
+);
+
 -- 5
 CREATE TABLE FORNECIVEIS (
-	fnv_id            SERIAL       NOT NULL,
-	fnv_tipo          CHAR(1)      NOT NULL,
-	fnv_codigo        VARCHAR(20)  NOT NULL,
-	fnv_descricao     VARCHAR(255) NOT NULL,
-	fnv_data_inicio   DATE         NOT NULL,
-	fnv_for_id        INTEGER      NOT NULL,
-	fnv_tipo_cadastro CHAR(1)      NOT NULL,
+	fnv_id              SERIAL       NOT NULL,
+	fnv_tipo            CHAR(1)      NOT NULL,
+	fnv_codigo          VARCHAR(20)  NOT NULL,
+	fnv_descricao       VARCHAR(255) NOT NULL,
+	fnv_for_id          INTEGER      NOT NULL,
+	fnv_tipo_fornecivel CHAR(1)      NOT NULL,
+	fnv_tipo_cadastro   CHAR(1)      NOT NULL,
 	CONSTRAINT PK_FNV PRIMARY KEY (fnv_id),
 	CONSTRAINT FK_FNV_FOR FOREIGN KEY (fnv_for_id)
 		REFERENCES FORNECEDORES (for_id),
 	CONSTRAINT UK_FNV_01 UNIQUE (fnv_codigo),
 	CONSTRAINT CK_FNV_01 CHECK (fnv_tipo_cadastro IN ('R','A','I')),
-	CONSTRAINT CK_FNV_02 CHECK (fnv_tipo_cadastro IN ('P','S'))
+	CONSTRAINT CK_FNV_02 CHECK (fnv_tipo_fornecivel IN ('P','S'))
 );
