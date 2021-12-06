6b36fd8cfb4d6c5e278432b2b91a633659464570
Rafael Cordeiro <rafaelcordeiro70@gmail.com>
2021-12-06 20:03:16 -0300

    [playground] test: Algoritmo de validação de CNPJ

--- sample-server-scm-fornecedores/sample-server/src/main/java/com/temperosoft/samplaserver/testsplayground/ValidationCnpj.java
+++ sample-server-scm-fornecedores/sample-server/src/main/java/com/temperosoft/samplaserver/testsplayground/ValidationCnpj.java
@@ -10,16 +10,66 @@ public class ValidationCnpj {
 	
 	public static void main(String[] args) {
 		String cnpj = sc.nextLine();
-		//11.222.333/0001-XX
-		List<Integer> digitos = new ArrayList<Integer>();
-		List<Integer> soma = new ArrayList<Integer>();
+		//11.222.333/0001-81
+		cnpj = cnpj.replace("/", "");
+		cnpj = cnpj.replace(".", "");
+		cnpj = cnpj.replace("-", "");
+		System.out.println(cnpj);
+		
+		int[] digitos = new int[15];
+		List<Integer> arraySoma = new ArrayList<Integer>();
 		
 		for (int i = 0; i < cnpj.length(); i++) {
-			if (i != 2 || i != 6 || i != 10 || i != 15)
-				digitos.add(Integer.getInteger(cnpj.substring(i,i+1)));
+			digitos[i] = cnpj.charAt(i) - 48;
 		}
 		
-//		digitos.get(arg0)
+		arraySoma.add(digitos[0] * 5);
+		arraySoma.add(digitos[1] * 4);
+		arraySoma.add(digitos[2] * 3);
+		arraySoma.add(digitos[3] * 2);
+		arraySoma.add(digitos[4] * 9);
+		arraySoma.add(digitos[5] * 8);
+		arraySoma.add(digitos[6] * 7);
+		arraySoma.add(digitos[7] * 6);
+		arraySoma.add(digitos[8] * 5);
+		arraySoma.add(digitos[9] * 4);
+		arraySoma.add(digitos[10] * 3);
+		arraySoma.add(digitos[11] * 2);
+		
+		int soma1 = arraySoma.stream().reduce(0, Integer::sum);
+		int resto1 = soma1 % 11;
+		digitos[12] = (resto1 < 2) ? 0 : 11 - resto1;
+		
+		
+		arraySoma = new ArrayList<Integer>();
+		
+		arraySoma.add(digitos[0] * 6);
+		arraySoma.add(digitos[1] * 5);
+		arraySoma.add(digitos[2] * 4);
+		arraySoma.add(digitos[3] * 3);
+		arraySoma.add(digitos[4] * 2);
+		arraySoma.add(digitos[5] * 9);
+		arraySoma.add(digitos[6] * 8);
+		arraySoma.add(digitos[7] * 7);
+		arraySoma.add(digitos[8] * 6);
+		arraySoma.add(digitos[9] * 5);
+		arraySoma.add(digitos[10] * 4);
+		arraySoma.add(digitos[11] * 3);
+		arraySoma.add(digitos[12] * 2);
+		
+		int soma2 = arraySoma.stream().reduce(0, Integer::sum);
+		int resto2 = soma2 % 11;
+		digitos[13] = (resto2 < 2) ? 0 : 11 - resto2;
+		
+		System.out.println(digitos[12] + "" +  digitos[13]);
+		
+		if (cnpj.charAt(cnpj.length()-2) != digitos[12] + 48)
+			System.out.println("NÃO");
+		
+		if (cnpj.charAt(cnpj.length()-1) != digitos[13] + 48)
+			System.out.println("NÃO");
+		
+		System.out.println();
 		
 	}
 }
